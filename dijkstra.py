236bb7bee442e0f5c0347080b4d0d7ee604dabac
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2020-04-05 15:41:52 -0400

    Add files via upload

--- dijkstra.py
+++ dijkstra.py
@@ -1,52 +1,12 @@
 '''
 Function should look like: dijkstra(graph, start, end)
-Should move from print path A->B->C and distance number
+print From A to B: path = A->B->C and distance number
 if end = end - 1
 path.append()
 '''
 
 import sys
-
-'''
-# Function that implements Dijkstra's single source shortest path using a 2D array to represent an Adjacency Matrix
-def dijkstra(graph, start): 
-    vertexes = len(graph)  # -1 or +1, do we start at 0 in the graph?
-    distance = [sys.maxsize+1] * vertexes  # Initialize distance super far, so unreachable (sys.maxsize+1)
-    distance[start] = 0   # Initialize source to be 0
-    visited = [False] * vertexes  # Visited array initialized to not visited
-
-    for i in range(vertexes):
-        minDistance = sys.maxsize + 1  # Impossible to be at, so its automatically the largest
-        for length in range(vertexes):  # Return smallest distance of a row
-            if distance[length] < minDistance and visited[length] is False:
-                minDistance = distance[length]
-                vertex = length  # vertex = the index of the smallest distance in a row(vertex)
-        print("u = " + str(vertex) + " | minDinstance(vertexes=" + str(vertexes) + ", distance=" + str(distance) + ", visited=" + str(visited))
-
-        visited[vertex] = True  # Mark vertex as visited
-
-        for length in range(vertexes):  # For each distance in a row(vertex)
-            if graph[vertex][length] > 0 and visited[length] is False and distance[length] > distance[vertex] + graph[vertex][length]:
-                distance[length] = distance[vertex] + graph[vertex][length]  # Add smallest distances
-    for node in range(vertexes):
-        print(node, distance[node])
-
-
-example_graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
-                 [4, 0, 8, 0, 0, 0, 0, 11, 0],
-                 [0, 8, 0, 7, 0, 4, 0, 0, 2],
-                 [0, 0, 7, 0, 9, 14, 0, 0, 0],
-                 [0, 0, 0, 9, 0, 10, 0, 0, 0],
-                 [0, 0, 4, 14, 10, 0, 2, 0, 0],
-                 [0, 0, 0, 0, 0, 2, 0, 1, 6],
-                 [8, 11, 0, 0, 0, 0, 1, 0, 7],
-                 [0, 0, 2, 0, 0, 0, 6, 7, 0]]
-
-vertexes = 9
-
-#adjacentMatrix, vertex#, source vertex/row
-dijkstra(example_graph, 0)
-'''
+path = []
 
 # Function that implements Dijkstra's single source shortest path using a 2D array to represent an Adjacency Matrix
 def dijkstra(graph, start, end):
@@ -54,7 +14,7 @@ def dijkstra(graph, start, end):
     distance = [sys.maxsize+1] * vertexes  # Initialize distance super far, so unreachable (sys.maxsize+1)
     distance[start] = 0   # Initialize source to be 0
     visited = [False] * vertexes  # Visited array initialized to not visited
-    path = []  # Stores the smallest distance
+    parent = [-1] * vertexes  # Parent array of previous vertexes to store shortest path tree, see:  path[dist] = vertex
 
     for nextVertex in range(vertexes):  # For all vertexes
         minDistance = sys.maxsize + 1  # Impossible to be at, so its automatically the largest
@@ -66,12 +26,26 @@ def dijkstra(graph, start, end):
 
         visited[vertex] = True  # Mark vertex as visited
 
-        for length in range(vertexes):  # For each distance in a row(vertex)
-            if graph[vertex][length] > 0 and visited[length] is False and distance[length] > distance[vertex] + graph[vertex][length]:
-                distance[length] = distance[vertex] + graph[vertex][length]  # Add smallest distances
+        ########################################## Why does this part work ##########################################################
+        for dist in range(vertexes):  # For each distance in a row(vertex)
+            if graph[vertex][dist] > 0 and visited[length] is False and distance[dist] > distance[vertex] + graph[vertex][dist]:
+                distance[dist] = distance[vertex] + graph[vertex][dist]  # Add smallest distance
+                parent[dist] = vertex  ####### Why does this line work #######
+        #############################################################################################################################
+
+    def getPath(parent, j):  # Recurse through parent array and append to path
+        global path
+        if parent[j] == -1:
+            path.append(j)
+            return
+        getPath(parent, parent[j])
+        path.append(j)
+
+    getPath(parent, end-1)  # Execute getPath here, for start to end
+    print(path)
 
     for node in range(vertexes):
-        print(node, distance[node])
+        print("Vertex = "+str(node)+"|", distance[node])
 
 
 example_graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
@@ -88,3 +62,5 @@ vertexes = 9
 
 #adjacentMatrix, vertex#, source vertex/row
 dijkstra(example_graph, 0, 9)
+
+# path for 0 to 9: 0->1->2->8
