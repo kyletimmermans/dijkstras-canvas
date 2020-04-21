e92476e55e5b18ab4c216c49891936bb21190ede
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2020-04-21 16:01:49 -0400

    Add files via upload

--- Class.py
+++ Class.py
@@ -2,6 +2,7 @@ from tkinter import *
 from math import sqrt
 
 # This point lies on the edge of the circle so there's no line overhang on the green vertex (cuts off extra overlap)
+# https://math.stackexchange.com/questions/127613/closest-point-on-circle-edge-from-point-outside-inside-the-circle
 class circleEdgePoint:
     def __init__(self, x1, y1, x2, y2, r):
         self.x1 = x1
@@ -10,15 +11,11 @@ class circleEdgePoint:
         self.y2 = y2
         self.r = r
     def cx(self):
-        return self.x1 + (self.r * (self.x2 - self.x1) / sqrt((self.x2 - self.x1) + (self.y2 - self.y1)))  # Needs the multiplication asterisk
+        return self.x1 + (self.r * (self.x2 - self.x1) / sqrt((self.x2 - self.x1)**2 + (self.y2 - self.y1)**2 ))  # Needs the multiplication asterisk
     def cy(self):
-        return self.y1 + (self.r * (self.y2 - self.y1) / sqrt((self.x2 - self.x1) + (self.y2 - self.y1)))
+        return self.y1 + (self.r * (self.y2 - self.y1) / sqrt((self.x2 - self.x1)**2 + (self.y2 - self.y1)**2 ))
     def final(self):
         return [round(self.cx(), 1), round(self.cy(), 1)]  # Return x,y of C-coord as a list, round() to nearest tenths place, e.g. 7.1
-
-points = circleEdgePoint(5, 10, 15, 20, 5)   # Example points
-print(points.final())  # need to use member function .final() to get the points
-
 
 root = Tk()
 root.title("Dijkstra's Canvas - @KyleTimmermans")
@@ -30,8 +27,10 @@ circle1 = draw_space.create_oval(x0 - 25, y0 - 25, x0 + 25, y0 + 25, fill='Green
 circle2 = draw_space.create_oval(x1 - 25, y1 - 25, x1 + 25, y1 + 25, fill='Red', tags='vertex')
 
 # The Math Magic is going to be here #
-
-line = draw_space.create_line(x0-22, y0-10, x1+22, y1+10, fill='Black', width=5, tags='edge')  # Do the inverse of what you did to x0,y0 to x1,y1 (at least in this case)
+points = circleEdgePoint(700, 700, 400, 500, 25).final()
+points2 = circleEdgePoint(400, 500, 700, 700, 25).final()  # Now for going the other way around
+line = draw_space.create_line(points[0], points[1], points2[0], points2[1], fill='Black', width=5, tags='edge')
+#line = draw_space.create_line(x0-22, y0-10, x1+22, y1+10, fill='Black', width=5, tags='edge')  # Do the inverse of what you did to x0,y0 to x1,y1 (at least in this case)
 draw_space.pack()
 
 root.mainloop()
